dcc76d12be92a6da34c76a05c34e8302e8006cf5
Hugo <hugo.marello@tst.jus.br>
2019-10-08 19:23:22 -0300

    fixing e2e tests

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getTitleText() {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+    return element(by.css('app-root app-side-menu mat-toolbar span')).getText() as Promise<string>;
   }
 }
